7c59b8fea161b6977c45fc9c5a3108abf9cbf64a
lli205 <116689575+lli205@users.noreply.github.com>
2023-10-03 17:07:53 -0500

    Initial Commit

---
+++ Flags/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Flags
+//
+//  Created by Litao Li on 10/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Litao Li on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
